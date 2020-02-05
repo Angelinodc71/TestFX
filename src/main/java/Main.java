a1e3ed8f9687dfdba46f2c1178195c16ee989e8e
angelinodc71 <adec58@elpuig.xeill.nety>
2020-02-05 19:46:35 +0100

    ListView rulando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import control.Film;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -13,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("sample.fxml")));
         stage.setTitle("HOLA GUAPO");
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
--- src/main/java/control/SampleController.java
+++ src/main/java/control/SampleController.java
@@ -1,8 +1,13 @@
 package control;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.input.MouseEvent;
 
 import javax.xml.bind.JAXBContext;
@@ -17,6 +22,9 @@ import java.util.ResourceBundle;
 
 public class SampleController implements Initializable {
     int i = 0;
+    ObservableList<String> observableList = FXCollections.observableArrayList();
+    @FXML
+    ListView<String> listView;
     @FXML
     Button btn001;
 
@@ -24,12 +32,11 @@ public class SampleController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
             metodo();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JAXBException e) {
+        } catch (IOException | JAXBException e) {
             e.printStackTrace();
         }
         i=0;
+
     }
 
     public void onClick(MouseEvent mouseEvent) throws IOException, JAXBException {
@@ -37,6 +44,7 @@ public class SampleController implements Initializable {
 //
 //        }
         btn001.setText(String.valueOf(films.get(i++)));
+
     }
 
     private List<Film> films;
@@ -48,5 +56,7 @@ public class SampleController implements Initializable {
         JAXBContext jaxbContext = JAXBContext.newInstance(Films.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
         this.films = ((Films)jaxbUnmarshaller.unmarshal(is)).films;
+        listView.setItems(observableList);
+        observableList.addAll(String.valueOf(films));
     }
 }
